c2b6e9192ce04a93a21c9dcdcf0ec86bce294f14
todocodeacademy <admin@todocodeacademy.com>
2023-04-02 22:15:58 -0300

    commit sql

---
+++ 1-insertPacientesTodoCode.sql
@@ -0,0 +1,12 @@
+INSERT INTO Pacientes (id_paciente, nombre_paciente, fecha_nacimiento, especie)
+VALUES
+  (1, 'Milo', '2019-05-02', 'Perro'),
+  (2, 'Luna', '2020-02-14', 'Gato'),
+  (3, 'Rocky', '2018-10-11', 'Perro'),
+  (4, 'Kitty', '2021-01-23', 'Gato'),
+  (5, 'Max', '2017-08-04', 'Perro'),
+  (6, 'Simba', '2020-11-30', 'Gato'),
+  (7, 'Toby', '2016-03-15', 'Perro'),
+  (8, 'Coco', '2019-07-19', 'Gato'),
+  (9, 'Lola', '2015-12-25', 'Perro'),
+  (10,'Socks', '2021-02-28', 'Gato');
---
+++ 2-insertTratamientosTodoCode.sql
@@ -0,0 +1,40 @@
+INSERT INTO Tratamientos (id_tratamiento, id_paciente, tipo_tratamiento, fecha_tratamiento)
+VALUES 
+  
+  (1, 2, 'Vacunación', '2022-03-01'),
+  (2, 2, 'Desparasitación', '2022-03-15'),
+  (3, 2, 'Chequeo general', '2022-04-02');
+  (4, 1, 'Vacunación', '2022-03-01'),
+  (5, 1, 'Desparasitación', '2022-03-15'),
+  (6, 1, 'Chequeo general', '2022-04-02'),
+  (7, 1, 'Tratamiento dental', '2022-05-10'),
+  (8, 4, 'Vacunación', '2022-04-01'),
+  (9, 4, 'Desparasitación', '2022-04-15'),
+  (10, 4, 'Chequeo general', '2022-05-02'),
+  (11, 4, 'Tratamiento de heridas', '2022-06-10'),
+  (12, 7, 'Vacunación', '2022-04-05'),
+  (13, 7, 'Desparasitación', '2022-04-20'),
+  (14, 7, 'Chequeo general', '2022-05-03'),
+  (15, 7, 'Tratamiento para alergias', '2022-06-12'),
+  (16, 9, 'Vacunación', '2022-05-01'),
+  (17, 9, 'Desparasitación', '2022-05-15'),
+  (18, 9, 'Chequeo general', '2022-06-02'),
+  (19, 9, 'Tratamiento para infección de oídos', '2022-07-10'),
+  (20, 10, 'Vacunación', '2022-05-05'),
+  (21, 10, 'Desparasitación', '2022-05-20'),
+  (22, 10, 'Chequeo general', '2022-06-03'),
+  (23, 10, 'Tratamiento para problemas de piel', '2022-07-15'),
+  (24, 3, 'Vacunación', '2022-03-10'),
+  (25, 3, 'Desparasitación', '2022-03-25'),
+  (26, 3, 'Chequeo general', '2022-04-12'),
+  (27, 3, 'Tratamiento para pulgas y garrapatas', '2022-05-20'),
+  (28, 4, 'Limpieza dental', '2022-01-01'),
+  (29, 4, 'Desparasitación', '2022-02-03'),
+  (30, 4, 'Vacunación', '2022-03-05'),
+  (31, 4, 'Extracción de dientes', '2022-04-07'),
+  (32, 4, 'Tratamiento de la piel', '2022-05-09'),
+  (33, 4, 'Radiografía', '2022-06-11'),
+  (34, 4, 'Cirugía', '2022-07-13');
+
+  
+
